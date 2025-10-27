3a0ab25145220bf7da70b6da26f3367b14679847
Mafurios <jeroen_vank_nr1@hotmail.com>
2025-10-27 16:46:35 +0100

    A002: CH2L4 main.py add function word_counter

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def get_book_text(file_path):
         text = content.read()
     return text
 
+def word_counter(text):
+    num_words = len(text.split())
+    return num_words
+
 def main():
     file_path = "./books/frankenstein.txt"
     text = get_book_text(file_path)
-    print(text)
+    num_words = word_counter(text)
+    print(f"Found {num_words} total words")
 
 main()
