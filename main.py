54957fa8cff1548833abe8b57eef91676ac3b046
Mafurios <jeroen_vank_nr1@hotmail.com>
2025-10-27 16:58:40 +0100

    A003: CH2L5 add stats.py word_counter moved to stats.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import word_counter
+
 def get_book_text(file_path):
     with open(file_path) as content:
         text = content.read()
     return text
-
-def word_counter(text):
-    num_words = len(text.split())
-    return num_words
 
 def main():
     file_path = "./books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_counter(text):
+    num_words = len(text.split())
+    return num_words
