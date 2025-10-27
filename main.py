a1d8a323db1cbe34765ec8930577acddd7e85fb6
Mafurios <jeroen_vank_nr1@hotmail.com>
2025-10-27 17:40:05 +0100

    A004: CH2L6 update stats.py add character_counter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import word_counter
+from stats import word_counter, character_counter
 
-def get_book_text(file_path):
+def get_book_text(file_path): # open the book and get the full content
     with open(file_path) as content:
         text = content.read()
     return text
@@ -9,6 +9,8 @@ def main():
     file_path = "./books/frankenstein.txt"
     text = get_book_text(file_path)
     num_words = word_counter(text)
+    num_characters = [character_counter(text)]
+    print(num_characters)
     print(f"Found {num_words} total words")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
-def word_counter(text):
+def word_counter(text): # take book content and count all words
     num_words = len(text.split())
     return num_words
+
+def character_counter(text): # take book content and count all characters ignoring capitalization
+    num_characters = {}
+
+    lowered_text = text.lower()
+    for character in list(lowered_text):
+        if character in num_characters:
+            num_characters[character] += 1
+        else:
+            num_characters[character] = 1
+    return num_characters
